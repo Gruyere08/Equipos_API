8a7b8891e6a5dea118dab79fcabebda4ce773870
Jacob <athiaguin.es@gmail.com>
2026-04-18 12:51:06 -0300

    Add base Spring Boot project structure

---
+++ src/main/java/com/example/Equipos_API/EquiposApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Equipos_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiposApiApplication.class, args);
+	}
+
+}
