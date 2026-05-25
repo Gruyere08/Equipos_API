935a197e458a59d91d38006df6427b1125f51733
Jacob <athiaguin.es@gmail.com>
2026-05-25 13:38:31 -0300

    Implement handleValidationExceptions

--- src/main/java/com/example/Equipos_API/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Equipos_API/exception/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package com.example.Equipos_API.exception;
 
 import com.example.Equipos_API.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 // GlobalExceptionHandler.java
 @RestControllerAdvice
@@ -15,4 +18,17 @@ public class GlobalExceptionHandler {
     public ErrorResponse handleEquipoNotFound(EquipoNotFoundException e) {
         return new ErrorResponse(e.getMessage(), 404);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException e) {
+        String mensaje = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return new ErrorResponse(mensaje, 400);
+    }
+
 }
